dd2530113e84552697ab11bc60ee20c94743484f
Anastasia <n41111215@mail.ru>
2020-06-26 17:19:55 +0300

    Add ProductManager.java

--- src/main/java/ru/netology/manager/ProductManager.java
+++ src/main/java/ru/netology/manager/ProductManager.java
@@ -28,6 +28,19 @@ public class ProductManager {
         return result;
     }
 
+    public void removeById(int id) {
+        repository.removeById(id);
+        System.out.println("manager done");
+    }
 
+    public Product[] getAll() {
+        Product[] items = repository.findAll();
+        Product[] result = new Product[items.length];
+        for (int i = 0; i < result.length; i++) {
+            int index = items.length - i - 1;
+            result[i] = items[index];
+        }
+        return result;
+    }
 }
 
